2dacf3fe38937eb41de4bf4a5b04da9311a0f7a2
Cyber Oliveira <lccro@msn.com>
2020-02-29 21:19:43 -0300

    initial version

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Clock/>
+  </div>
+</template>
+
+<script>
+  import Clock from './components/Clock.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Clock
+    }
+  }
+</script>
+
+<style>
+  html {
+    background: rgba(0, 0, 0, 0.5) url('https://source.unsplash.com/random') no-repeat scroll center center;
+    background-size: cover;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-blend-mode: darken;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/Clock.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="clock">
+    {{ time }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Clock',
+    data() {
+      return {
+        timer: null,
+        time: "⏱"
+      }
+    },
+    mounted() {
+      this.timer = setInterval(this.updateClock, 1000)
+    },
+    destroyed() {
+      clearInterval(this.timer)
+    },
+    methods: {
+      updateClock() {
+        const now = new Date()
+        this.time = now.toLocaleTimeString('pt-BR')
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .clock {
+    color: white;
+    font-size: 10rem;
+    position: fixed;
+    left: 40px;
+    bottom: 10px;
+  }
+</style>
